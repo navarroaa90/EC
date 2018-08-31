779c4b53d7dd3056f8ddd671be01427d5c011849
Antonio Navarro <navarroaa90@gmail.com>
2018-08-31 10:58:35 -0700

    added some pages and sessions login

--- server.js
+++ server.js
@@ -4,8 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+
+// connect to database 
+mongoose.connect(config.database);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
 
 var index = require('./routes/index');
+var adminPages = require('./routes/admin_pages');
 var users = require('./routes/users');
 
 var app = express();
@@ -19,11 +33,47 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Express Session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+// Express Validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root   = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParm += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg :msg,
+      value: value
+    };
+  }
+}));
+
+// express messages & connect flash middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin/pages', adminPages);
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
