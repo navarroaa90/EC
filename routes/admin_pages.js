3c0ac6357669a579c89d5f05b96b91f2bfde6d29
Antonio Navarro <navarroaa90@gmail.com>
2018-09-02 12:47:10 -0700

    added Post page and error messages

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,9 +1,75 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+// GET page model 
+var Page = require('../models/page');
+
+/* GET pages index. */
+router.get('/', function(req, res) {
+  res.send('admin area');
+});
+
+/* GET pages index. */
+router.get('/add-page', function(req, res) {
+  var title = "";
+  var slug = "";
+  var content = "";
+  
+  res.render('admin/add_page', {
+      title: title,
+      slug: slug,
+      content: content 
+  });
+});
+
+/* POST pages index. */
+router.post('/add-page', function(req, res) {
+
+  req.checkBody('title', 'Title must have value.').notEmpty();
+  req.checkBody('content', 'Content must have value.').notEmpty();
+  
+  var title = req.body.title;
+  var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+  if (slug == "") slug = title.replace(/\s+/g, '-').toLowerCase();
+  var content = req.body.content;
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    
+    res.render('admin/add_page', {
+        errors: errors,
+        title: title,
+        slug: slug,
+        content: content 
+    });
+  } else {
+    Page.findOne({slug: slug}, function (err, page) {
+        if (page) {
+          req.flash('danger', 'Page slug exists, choose another.');
+          
+    res.render('admin/add_page', {
+      title: title,
+      slug: slug,
+      content: content 
+  });
+        } else {
+          var page = new Page ( {
+            title: title,
+            slug: slug,
+            content: content,
+            sorting: 0
+          });
+          page.save(function(err) {
+            if (err) return console.log(err);
+
+            req.flash('success', 'Page added!');
+            res.redirect('/admin/pages');
+          });
+        }
+    });
+  }
+
 });
 
 module.exports = router;
