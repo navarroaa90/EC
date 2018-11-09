477a7aab4588b11293ea23e58c79ff8a134cb0d7
Antonio Navarro <navarroaa90@gmail.com>
2018-11-09 13:19:37 -0800

    fixed error with database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.DATABASE_URL, {useMongoClient: true});
+mongoose.connect(process.env.DATABASE_URL);
 
 mongoose.connection.once('open', function () {
   console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
